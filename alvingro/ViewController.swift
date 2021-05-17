2bbc72d1e9ee28637208423547f4108021059ff7
Nguyen <nguyennguyen121198@gmail.com>
2021-05-17 11:05:46 +0700

    create project

---
+++ alvingro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alvingro
+//
+//  Created by Thành Nguyên on 17/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(named: "MainGreen")
+    }
+
+
+}
+
