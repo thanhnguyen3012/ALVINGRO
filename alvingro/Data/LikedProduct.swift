066f64fc2900eb2248a8d64f12ac763244b5a0b5
Nguyen <nguyennguyen121198@gmail.com>
2021-06-01 15:43:22 +0700

    feat(FavoriteView) make the view work with realmdb

---
+++ alvingro/Data/LikedProduct.swift
@@ -0,0 +1,8 @@
+//
+//  LikedProduct.swift
+//  alvingro
+//
+//  Created by Thành Nguyên on 01/06/2021.
+//
+
+import Foundation
---
+++ alvingro/Extension/UITableView+setEmptyMessage.swift
@@ -0,0 +1,8 @@
+//
+//  UITableView+setEmptyMessage.swift
+//  alvingro
+//
+//  Created by Thành Nguyên on 01/06/2021.
+//
+
+import Foundation
